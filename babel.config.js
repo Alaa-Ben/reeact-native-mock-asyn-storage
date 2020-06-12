3b6d3ec058aa6a515568607c3142ec20a687b6fc
Alaa Ben El Ahmar <alaa.benelahmar@qucit.com>
2020-06-12 14:17:39 +0200

    added same packages as real repo

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+  'presets': ['module:metro-react-native-babel-preset'],
+  'env': {
+    'production': {
+      'plugins': ['transform-remove-console']
+    }
+  },
+  'plugins': [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-modules-commonjs'
+  ]
+}
